99440625417507b84db0a9affec830581b3b3cbf
Vanessa-PAC <58774100+Vanessa-PAC@users.noreply.github.com>
2020-10-13 16:54:37 +0200

    'initial'

--- src/App.js
+++ src/App.js
@@ -4,22 +4,57 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // 
+<body>
+<div class="container-fluid bg-light">
+ <ul class="nav justify-content-end">
+  <li class="nav-item">
+    <a class="nav-link " href="#">Livraison standard gratuite pour toute commande supérieure à 90€</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Trouver un point de vente</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Suivi de commande</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link"  href="#">S'identifier'</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link"  href="#">Mon panier</a>
+  </li>
+ </ul>
+</div>  
+   
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a class="navbar-brand" href="#">Hidden brand</a>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link"  href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div class="jumbotron jumbotron-fluid">
+<img src="" class="img-fluid" alt="responsive image"></img>
+</div>
+
+</body>
   );
 }
 
